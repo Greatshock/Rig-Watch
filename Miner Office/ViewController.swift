36fb179fdbc4ec2d7399ca297e8f0c66befe263d
Nikita Marinosyan <nikita.marinosyan@gmail.com>
2018-01-08 14:46:08 +0300

    Initial Commit

---
+++ Miner Office/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Miner Office
+//
+//  Created by Nikita Marinosyan on 08.01.2018.
+//  Copyright © 2018 Nikita Marinosyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
